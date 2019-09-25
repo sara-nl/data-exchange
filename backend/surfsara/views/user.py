417431465ce0eb4e0e48c7f9d17ae4cc722020b0
Sijmen Schoon <me@sijmenschoon.nl>
2019-09-25 11:00:06 +0200

    Add some documentation to UserViewSet

--- backend/surfsara/views/user.py
+++ backend/surfsara/views/user.py
@@ -4,10 +4,17 @@ from rest_framework.decorators import api_view
 from surfsara.models import User
 
 
+"""
+Create/Read/Update/Delete view for the User model.
+This creates the API endpoints for /users/.
+"""
 class UserViewSet(viewsets.ModelViewSet):
     class Serializer(serializers.HyperlinkedModelSerializer):
         class Meta:
             model = User
+
+            # Specify the fields that are sent to the user
+            # (to avoid sending e.g. the password hash to a client).
             fields = ["url", "username", "email", "is_staff", "webdav_username"]
 
     queryset = User.objects.all()

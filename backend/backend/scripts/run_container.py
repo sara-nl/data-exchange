440d641230f2d6be153a28447459bebe7ec31299
Sander van Wickeren <sander@bit-students.com>
2019-09-25 22:37:46 +0200

    PEP8 and first test rewritten using RDclient

--- backend/backend/scripts/run_container.py
+++ backend/backend/scripts/run_container.py
@@ -7,23 +7,27 @@ import argparse
 import time
 import docker
 
-from rd_connector import download_file
+from .rd_connector import ResearchdriveClient
 
 
 class RunContainer:
-    def __init__(self, algorithm_file_name, data_file_name, download_dir=""):
-        self.algorithm_file_name = algorithm_file_name
-        self.data_file_name = data_file_name
-
+    def __init__(self, remote_algorithm_path, remote_data_path, download_dir=""):
+        self.remote_algorithm_path = remote_algorithm_path
+        self.remote_data_path = remote_data_path
         self.download_dir = download_dir
-
         self.container = None
 
-    def download_and_run(self, username, password):
+        self.temp_algorithm_file = None
+        self.temp_data_file = None
+        self.temp_algorithm_name = None
+        self.temp_data_name = None
+        self.temp_output_file = None
+
+    def download_and_run(self):
         """
             Combines downloaden and running of algorithm with data
         """
-        self.download_files(username, password)
+        self.download_files()
         self.run_algorithm()
 
     def run_algorithm(self):
@@ -41,7 +45,7 @@ class RunContainer:
         except (Exception, KeyboardInterrupt, EnvironmentError) as e:
             self.stop_running()
 
-    def download_files(self, username, password):
+    def download_files(self):
         """
             Downloads both the algorithm and data
 
@@ -52,7 +56,7 @@ class RunContainer:
             print("=== Downloading files ===")
 
             self.create_files()
-            self.download_from_rd(username, password)
+            self.download_from_rd()
 
             print("Changed file permissions to read-only")
             # change permissions of files
@@ -139,29 +143,17 @@ class RunContainer:
         self.temp_algorithm_name = self.temp_algorithm_file.split("/")[-1]
         self.temp_data_name = self.temp_data_file.split("/")[-1]
 
-    def download_from_rd(self, username, password):
+    def download_from_rd(self):
         """
             Downloads both the algorithm and data from researchdrive
         """
 
-        options = {
-            "webdav_hostname": "https://researchdrive.surfsara.nl",
-            "webdav_root": "/remote.php/nonshib-webdav/",
-            "webdav_login": username,
-            "webdav_password": password,
-        }
+        rd_client = ResearchdriveClient()
 
-        if self.algorithm_file_name and self.data_file_name:
+        if self.remote_algorithm_path and self.remote_data_path:
             try:
-                download_file(
-                    options,
-                    filename=self.algorithm_file_name,
-                    filepath=self.temp_algorithm_file,
-                )
-
-                download_file(
-                    options, filename=self.data_file_name, filepath=self.temp_data_file
-                )
+                rd_client.download(self.remote_algorithm_path, self.download_dir, self.temp_algorithm_file)
+                rd_client.download(self.remote_data_path, self.download_dir, self.temp_data_file)
             except FileNotFoundError as e:
                 self.stop_running(e)
         else:
@@ -234,4 +226,5 @@ if __name__ == "__main__":
 
     # run_env.download_files(args.username, args.password)
     # run_env.run_algorithm()
-    run_env.download_and_run(args.username, args.password)
+    run_env.download_and_run()
+    #un_env.create_files()

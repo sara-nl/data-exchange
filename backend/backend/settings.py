60373da0462f76b90d03ae37da825dd47069c733
Sijmen Schoon <me@sijmenschoon.nl>
2019-09-29 17:19:25 +0200

    Add new domain to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = "7^j#=nisv%m3%m_kx=ro54dc71!r+(9juh%p=r9t-+76-*&d^i"
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = ["test-2.dataex-sara.surf-hosted.nl", "localhost"]
+ALLOWED_HOSTS = [
+    "test-2.dataex-sara.surf-hosted.nl",
+    "dataexchange.surfsara.nl",
+    "localhost",
+]
 
 
 # Application definition

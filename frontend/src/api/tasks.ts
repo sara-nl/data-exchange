5f8ef281bee47da99b755601582fe366647d1227
Tijs Teulings <tijs.t@hotmail.com>
2019-10-03 20:19:37 +0200

    pushed last bits

---
+++ frontend/src/api/tasks.ts
@@ -0,0 +1,45 @@
+import { AxiosResponse, Method } from "axios";
+import Controller from "./controller";
+
+export class TasksStartRequest {
+    public algorithm: string = "";
+    public data_owner: string = "";
+    public requested_data: string = "";
+}
+
+export class TasksReviewRequest {
+    public data: Object = {};
+    public approved: boolean = false;
+}
+
+export class TasksReleaseRequest {
+    public data: Object = {};
+    public released: boolean = false;
+}
+
+export class TaskRetrieveRequest {
+    public task_id: number = 0;
+}
+
+
+export default class Tasks extends Controller {
+    public static async start(data: TasksStartRequest): Promise<AxiosResponse> {
+        return this.client.post("/tasks/", data);
+    }
+
+    public static async get(): Promise<AxiosResponse> {
+        return this.client.get("/tasks/")
+    }
+
+    public static async retrieve(id: number): Promise<AxiosResponse> {
+        return this.client.get(`/tasks/${id}`)
+    }
+
+    public static async review(id: number, data: TasksReviewRequest): Promise<AxiosResponse> {
+        return this.client.post(`/tasks/${id}/review/`, data)
+    }
+
+    public static async release(id: number, data: TasksReviewRequest): Promise<AxiosResponse> {
+        return this.client.post(`/tasks/${id}/release/`, data)
+    }
+}
---
+++ frontend/src/routes/task.svelte
@@ -0,0 +1,185 @@
+<script lang="ts">
+    import LoadFiles from "../api/loader";
+    import Tasks from "../api/tasks";
+
+    let task_id = 23
+
+
+    let to_approve_requests = []
+    let own_datasets = ["Pietje"]
+
+    let data = {
+        data_file: "",
+        updated_request: {},
+        approved: false,
+        released: false
+    }
+
+
+    let task;
+    let owner = false
+
+    let state = "Loading..."
+    let requester = "Loading..."
+    let data_owner = "Loading..."
+    let algorithm = "Loading..."
+    let dataset = "Loading..."
+    let dataset_desc = "Loading..."
+    let output;
+
+
+    getTask()
+    getUserFiles()
+
+    async function getUserFiles(){
+        try {
+            let { data: response } = await LoadFiles.start();
+            own_datasets = response.output.own_datasets;
+        } catch (error) {
+            console.log(error.toString())
+        }
+
+        return false
+    }
+
+    async function getTask(){
+        try {
+            let { data: response } = await Tasks.retrieve(task_id);
+            task = response["task"]
+            owner = response["owner"]
+
+            state = task["state"]
+            requester = task["author_email"]
+            data_owner = task["approver_email"]
+            algorithm = task["algorithm"]
+            dataset = task["dataset"]
+            dataset_desc = task["dataset_desc"]
+            output = task["output"]
+
+
+        } catch (error) {
+            console.log(error.toString())
+        }
+
+        return false
+    }
+
+    async function review_request(approved: boolean) {
+
+        data.approved = approved
+        console.log(dataset)
+        task["dataset"] = dataset
+        data.updated_request = task
+
+        try {
+            let { data: response } = await Tasks.review(task["id"], data);
+            state = response["state"]
+        } catch (error) {
+            console.log(error.toString())
+        }
+
+    }
+
+    async function release_output(released: boolean) {
+        data.released = released
+
+        try {
+            let { data: response } = await Tasks.release(task["id"], data);
+            state = response["state"]
+        } catch (error) {
+            console.log(error.toString())
+        }
+
+    }
+</script>
+
+
+<svelte:head>
+    <title>My Files</title>
+</svelte:head>
+
+<h2 class="display-5">
+    Task {task_id}
+    <small class="text-muted">{state}</small>
+</h2>
+
+<div class="container">
+    <br>
+
+    <div class="row">
+        <div class="col">
+            <div class="my-5">
+                <h4>State</h4>
+                {state}
+            </div>
+            <div class="my-5">
+                <h4>Requester</h4>
+                {requester}
+                <h4>Data owner</h4>
+                {data_owner}
+            </div>
+            <div class="my-5">
+                <h4>Dataset description</h4>
+                {dataset_desc}
+            </div>
+            <div class="my-5">
+                <h4>Algorithm</h4>
+                {algorithm}
+                <h4>Dataset</h4>
+
+                {#if owner && state === "data_requested"}
+                    <select
+                        class="form-control"
+                        bind:value={dataset}
+                        id="data-file"
+                        >
+
+                        {#if own_datasets}
+                            <option value="">Select dataset</option>
+
+                            {#each own_datasets as file}
+                                <option value={file}>{file}</option>
+                            {/each}
+                        {:else}
+                            <option value="">No datasets available</option>
+                        {/if}
+                    </select>
+
+            {:else}
+                {dataset}
+            {/if}
+            </div>
+            {#if owner && state === "data_requested"}
+            <button
+                class="btn btn-success"
+                on:click={() => review_request(true)}>
+                Approve
+            </button>
+            <button
+                class="btn btn-danger"
+                on:click={() => review_request(false)}>
+                Reject
+            </button>
+            {/if}
+
+            {#if owner && state === "running"}
+            <button
+                class="btn btn-success"
+                on:click={() => release_output(true)}>
+                Release Output
+            </button>
+            <button
+                class="btn btn-danger"
+                on:click={() => release_output(false)}>
+                Reject
+            </button>
+            {/if}
+        </div>
+        <div class="col-xs-12 col-md-6 border">
+                <pre>
+                    {output || "No output (yet)…"}
+                </pre>
+            </div>
+    </div>
+
+</div>

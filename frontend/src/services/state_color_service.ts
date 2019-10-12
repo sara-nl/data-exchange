a9ec1fb8b7f33d5b21a3b962e7511514ca695962
Tijs Teulings <tijs.t@hotmail.com>
2019-10-12 14:32:54 +0200

    working permissions with filename

---
+++ frontend/src/services/state_color_service.ts
@@ -0,0 +1,12 @@
+
+
+// export function get_state_color(state: string){
+//     let state_color = {
+//         "request_rejected": "danger",
+//         "output_rejected": "warning",
+//         "output_released": "success",
+//         "running": "info"
+//     }
+
+//     return state_color[state];
+// }

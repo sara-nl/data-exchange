a3256a03b7f30a2aa789dcf281e2ab9eeabe9528
Mike Kotsur <miccots@gmail.com>
2019-10-24 10:37:34 +0200

    Making ContainerFlow code a bit more readable

--- tasker/tasker/src/Resources.scala
+++ tasker/tasker/src/Resources.scala
@@ -2,9 +2,13 @@ import java.nio.file.{Files, Path, Paths}
 import java.util.concurrent.Executors
 
 import cats.effect.{Blocker, IO, Resource}
-import container.ContainerEnv
+import clients.DockerContainer
+import config.TaskerConfig
+import container.{ContainerCommand, ContainerEnv}
 import container.ContainerEnv.Artifact
+import container.Ids.ImageId
 import org.apache.commons.io.FileUtils
+import cats.implicits._
 
 object Resources {
 
@@ -20,12 +24,16 @@ object Resources {
     acquire = IO(Files.createTempDirectory("datex_"))
   )(release = dir => IO(FileUtils.deleteDirectory(dir.toFile)))
 
-  def containerEnvironmentResource(
+  /**
+    * Resource of a container environment: temporary directories, where all files can be downloaded.
+    * Acquire: temp dir created.
+    * Release: temp dir recursively deleted.
+    */
+  def containerEnv(
     startContainerCmd: Messages.StartContainer
   ): Resource[IO, ContainerEnv] =
     tempDirResource.evalMap { tempHome =>
       import cats.implicits._
-      import cats.syntax._
       val hostCodePath = Paths.get(tempHome.toString, "code")
       val hostDataPath = Paths.get(tempHome.toString, "data")
       val hostOutPath = Paths.get(tempHome.toString, "out")
@@ -39,5 +47,29 @@ object Resources {
           outputArtifact = Artifact.output(hostOutPath)
         ).pure[IO]
     }
+
+  /**
+    * Resource of an image, which can be used for creating a container for running the algorithm.
+    * Acquire: If necessary - container is created, dependencies installed, image created.
+    * Release: If necessary - container and image removed.
+    */
+  def bakedImageResource(
+    containerEnv: ContainerEnv,
+    requirementsTxtOption: Option[Path]
+  ): Resource[IO, ImageId] = requirementsTxtOption match {
+    case Some(requirementsTxtContainerPath) =>
+      DockerContainer
+        .startedContainer(
+          containerEnv,
+          ContainerCommand.installDeps(requirementsTxtContainerPath),
+          TaskerConfig.docker.image
+        )
+        .evalMap(
+          containerId =>
+            DockerContainer.lastStatusIO(containerId) *> IO.pure(containerId)
+        )
+        .flatMap(containerId => DockerContainer.imageFromContainer(containerId))
+    case None => Resource.pure(TaskerConfig.docker.image)
+  }
 
 }
--- tasker/tasker/src/SecureContainerFlow.scala
+++ tasker/tasker/src/SecureContainerFlow.scala
@@ -1,189 +1,118 @@
-import java.nio.file.Path
-
-import Messages.ContainerOutput
+import Messages.{AlgorithmOutput, Done}
 import cats.effect._
 import clients.webdav.{Webdav, WebdavPath}
 import clients.DockerContainer
-import config.TaskerConfig
-import container.{ContainerCommand, ContainerEnv}
-import container.Ids.ImageId
+import container.{ContainerCommand, ContainerEnv, ContainerState, LogMessages}
 import dev.profunktor.fs2rabbit.model._
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import io.circe.syntax._
 import utils.FilesIO
-import cats.implicits._
+import config.TaskerConfig.concurrency.implicits.ctxShiftGlobal
+
 import scala.language.postfixOps
 
 class SecureContainerFlow(consumer: fs2.Stream[IO, AmqpEnvelope[String]],
                           publisher: AmqpMessage[String] => IO[Unit]) {
   import io.circe.generic.auto._
 
-  // TODO: handle error in installed dependencies
-  private def imageResource(
+  private def runAlgorithm(
     containerEnv: ContainerEnv,
-    requirementsTxtOption: Option[Path]
-  ): Resource[IO, ImageId] = requirementsTxtOption match {
-    case Some(requirementsTxtContainerPath) =>
-      DockerContainer
-        .startedContainer(
-          containerEnv,
-          ContainerCommand.installDeps(requirementsTxtContainerPath),
-          TaskerConfig.docker.image
-        )
-        .evalMap(
-          containerId =>
-            DockerContainer.lastStatusIO(containerId) *> IO.pure(containerId)
-        )
-        .flatMap(containerId => DockerContainer.imageFromContainer(containerId))
-    case None => Resource.pure(TaskerConfig.docker.image)
-  }
+    runAlgorithmCmd: ContainerCommand
+  ): IO[ContainerState] =
+    for {
+      logger <- Slf4jLogger.create[IO]
+      requirementsFileOption <- containerEnv.codeArtifact.requirementsFile
+      result <- Resources
+        .bakedImageResource(containerEnv, requirementsFileOption)
+        .use { imageId =>
+          DockerContainer
+            .startedContainer(containerEnv, runAlgorithmCmd, imageId)
+            .use(containerId => {
+              for {
+                stateAndCodeOption <- DockerContainer.lastStatusIO(containerId)
+                _ <- logger.info(
+                  s"$containerId execution stopped with ${stateAndCodeOption.toString}"
+                )
+                scriptOutput <- DockerContainer.outputStream(containerId)
+              } yield
+                stateAndCodeOption match {
+                  case Some((status, code)) =>
+                    ContainerState.Exited(code, status, scriptOutput)
+                  case None => ContainerState.Unknown
+                }
+            })
+        }
+    } yield result
+
+  private def processMessage(msg: Messages.StartContainer): IO[Done] =
+    for {
+      logger <- Slf4jLogger.create[IO]
+      done <- {
+        Resources.containerEnv(msg).use { containerEnv =>
+          val artifactsToBeDownloaded = Map(
+            WebdavPath(msg.codePath) -> containerEnv.codeArtifact,
+            WebdavPath(msg.dataPath) -> containerEnv.dataArtifact
+          )
+
+          for {
+            _ <- logger.info(s"Container environment: $containerEnv")
+            _ <- Webdav.downloadToHost(artifactsToBeDownloaded)
+            runAlgorithmCmd <- ContainerCommand.runWithStrace(containerEnv)
+            endState <- runAlgorithm(containerEnv, runAlgorithmCmd)
+            stdoutContent <- FilesIO
+              .readFileContent(containerEnv.outputArtifact.hostStdoutFilePath)
+            stderrContent <- FilesIO
+              .readFileContent(containerEnv.outputArtifact.hostStderrFilePath)
+            straceContent <- FilesIO
+              .readFileContent(containerEnv.outputArtifact.hostStraceFilePath)
+            _ <- logger.info(
+              LogMessages.largeOutputWithNewlines("STDOUT", stdoutContent)
+            )
+            _ <- logger.info(
+              LogMessages.largeOutputWithNewlines("STDERR", stderrContent)
+            )
+            _ <- logger.trace(
+              LogMessages.largeOutputWithNewlines("STRACE", straceContent)
+            )
+            algorithmOutput = AlgorithmOutput(
+              stdoutContent,
+              stderrContent,
+              straceContent
+            )
+          } yield
+            endState match {
+              case ContainerState.Exited(0, _, output) =>
+                val taskUserOutput =
+                  s"Container exited successfully\n$output\n$stdoutContent\n$stderrContent"
+                Messages.Done
+                  .success(msg.taskId, taskUserOutput, algorithmOutput)
+              case ContainerState.Exited(x, _, output) =>
+                val taskUserOutput =
+                  s"Container exited with an error. Status code: $x\n$output\n$stdoutContent\n$stderrContent"
+                Messages.Done
+                  .error(msg.taskId, taskUserOutput, algorithmOutput)
+              case ContainerState.Unknown =>
+                Messages.Done.error(
+                  msg.taskId,
+                  "Container ended up in an unknown state. Please contact the development team.",
+                  algorithmOutput
+                )
+            }
+        }
+      }
+    } yield done
 
   val flow: fs2.Stream[IO, Unit] =
     consumer
       .through(Codecs.messageDecodePipe)
       .evalMap {
         case Right(startContainerCmd) =>
-          Slf4jLogger.create[IO].flatMap { logger =>
-            Resources
-              .containerEnvironmentResource(startContainerCmd)
-              .use { containerEnv =>
-                val artifactsToBeDownloaded = Map(
-                  WebdavPath(startContainerCmd.codePath) -> containerEnv.codeArtifact,
-                  WebdavPath(startContainerCmd.dataPath) -> containerEnv.dataArtifact
-                )
-                import config.TaskerConfig.concurrency.implicits.ctxShiftGlobal
-
-                val containerOutputIO = for {
-                  _ <- logger.info(s"Container environment: $containerEnv")
-                  _ <- Webdav.downloadToHost(artifactsToBeDownloaded)
-                  requirementsFileOption <- containerEnv.codeArtifact.requirementsFile
-//                  _ <- {
-//                    requirementsFileOption match {
-//                      case Some(requirements) =>
-//                        for {
-//                          _ <- logger
-//                            .info(s"Installing requirements from $requirements")
-//                          installReqCmd <- ContainerCommand
-//                            .installDeps(requirements)
-//                          containerId <- DockerContainer
-//                            .createContainer(containerEnv, installReqCmd)
-//                          _ <- logger.info(
-//                            s"Container with installed dependencies: ${containerId}"
-//                          )
-//                          _ <- startContainer(containerId)
-//                          stateAndCode <- DockerContainer
-//                            .lastStatusIO(containerId)
-//                          _ <- logger.info(
-//                            s"Container $containerId execution stopped with ${stateAndCode.toString}"
-//                          )
-//                          imageId <- DockerContainer.createImage(containerId)
-//                          _ <- logger.info(
-//                            s"Image id with installed dependencies: $imageId"
-//                          )
-//                          scriptOutput <- DockerContainer
-//                            .outputStream(containerId)
-//                          _ <- logger.info(s"Container output: $scriptOutput")
-//                        } yield ()
-//                      case None =>
-//                        logger.info("There are no requirements to install")
-//                    }
-//                  }
-                  runAlgorithmCmd <- ContainerCommand
-                    .runWithStrace(containerEnv)
-                  results <- imageResource(containerEnv, requirementsFileOption)
-                    .use { imageId =>
-                      DockerContainer
-                        .startedContainer(
-                          containerEnv,
-                          runAlgorithmCmd,
-                          imageId
-                        )
-                        .use(containerId => {
-                          for {
-                            stateAndCodeOption <- DockerContainer
-                              .lastStatusIO(containerId)
-                            _ <- logger.info(
-                              s"$containerId execution stopped with ${stateAndCodeOption.toString}"
-                            )
-                            scriptOutput <- DockerContainer
-                              .outputStream(containerId)
-                          } yield (stateAndCodeOption, scriptOutput)
-                        })
-                    }
-                  (stateAndCodeOption, scriptOutput) = results
-//                  runAlgorithmCntId <- DockerContainer
-//                    .createContainer(containerEnv, runAlgorithmCmd)
-//                  _ <- logger.info(s"Created container $runAlgorithmCntId")
-//                  _ <- startContainer(runAlgorithmCntId)
-//                  stateAndCode <- DockerContainer
-//                    .lastStatusIO(runAlgorithmCntId)
-//                  _ <- logger.info(
-//                    s"Container $runAlgorithmCntId execution stopped with ${stateAndCode.toString}"
-//                  )
-//                  scriptOutput <- DockerContainer
-//                    .outputStream(runAlgorithmCntId)
-                  stdoutContent <- FilesIO.readFileContent(
-                    containerEnv.outputArtifact.hostStdoutFilePath
-                  )
-                  stderrContent <- FilesIO.readFileContent(
-                    containerEnv.outputArtifact.hostStderrFilePath
-                  )
-                  straceContent <- FilesIO.readFileContent(
-                    containerEnv.outputArtifact.hostStraceFilePath
-                  )
-                  _ <- logger.info(
-                    s"\n ----- Start of STDOUT -----\n $stdoutContent \n ----- End of STDOUT -----"
-                  )
-                  _ <- logger.info(
-                    s"\n ----- Start of STDERR -----\n $stderrContent \n ----- End of STDERR -----"
-                  )
-//                  _ <- removeContainer(runAlgorithmCntId)
-//                  _ <- logger.info(s"Removed container: $runAlgorithmCntId")
-                } yield
-                  (
-                    stateAndCodeOption,
-                    (
-                      s"$scriptOutput\n$stdoutContent\n$stderrContent",
-                      ContainerOutput(
-                        stdoutContent,
-                        stderrContent,
-                        straceContent
-                      )
-                    )
-                  )
-
-                containerOutputIO.flatMap {
-                  case (Some((_, 0)), (output, containerOutput)) =>
-                    val doneMsg =
-                      Messages.Done.success(
-                        startContainerCmd.taskId,
-                        output,
-                        containerOutput
-                      )
-                    publisher(
-                      AmqpMessage(doneMsg.asJson.spaces2, AmqpProperties())
-                    )
-                  case (
-                      Some((state, nonZeroCode)),
-                      (output, containerOutput)
-                      ) =>
-                    val doneMsg =
-                      Messages.Done.error(
-                        startContainerCmd.taskId,
-                        s"Docker container exited in state $state with code $nonZeroCode. \n $output",
-                        containerOutput
-                      )
-                    publisher(
-                      AmqpMessage(doneMsg.asJson.spaces2, AmqpProperties())
-                    )
-                  case other =>
-                    logger
-                      .error(
-                        s"Container state was $other. Should never happen."
-                      )
-                }
-              }
-          }
+          for {
+            doneMsg <- processMessage(startContainerCmd)
+            _ <- publisher(
+              AmqpMessage(doneMsg.asJson.spaces2, AmqpProperties())
+            )
+          } yield ()
         case Left(error) =>
           for {
             logger <- Slf4jLogger.create[IO]

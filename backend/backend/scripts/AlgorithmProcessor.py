fd91a15198fc8e5d2fa3d3a0be70d9f83c9b37e6
Sander van Wickeren <sander@bit-students.com>
2019-10-24 23:10:02 +0200

    Remove redundant comments and code

--- backend/backend/scripts/AlgorithmProcessor.py
+++ backend/backend/scripts/AlgorithmProcessor.py
@@ -4,8 +4,8 @@ from multiprocessing import Process
 import string
 import tempfile
 
-class AlgorithmProcessor:
 
+class AlgorithmProcessor:
     def __init__(self, algorithm_name, data_requester):
         self.algorithm_name = algorithm_name
         self.data_requester = data_requester
@@ -14,12 +14,6 @@ class AlgorithmProcessor:
         self.processes = []
         self.files = []
 
-    def create_temp_file(self):
-        return
-
-    def update_file_rights(self):
-        return
-
     def is_folder(self):
         """
         A folder can look like a file by giving the folder a name like folder.py.
@@ -27,38 +21,29 @@ class AlgorithmProcessor:
         :return: True or False if it's a folder.
         """
         shares = self.rd_client.get_shares(self.data_requester, self.algorithm_name)
-
-        if "directory" in shares[0]['mimetype']:
+        if "directory" in shares[0]["mimetype"]:
             return True
         return False
-
-    def download_file(self):
-        tempname = tempfile.NamedTemporaryFile(dir=os.getcwd(), delete=False).name
-        print(tempname)
-        print(files)
-        rd_client.download(self.algorithm_name + "/" + files[0], os.getcwd(), tempname)
-        return
 
     def update_database(self):
         return
 
-    def process(self):
+    def start_processing(self):
         if self.is_folder:
             for file in self.rd_client.list(self.algorithm_name):
                 remote_path = self.algorithm_name + "/" + file
-                algorithm_process = Process(target=self.process_algorithm, args=(remote_path,))
+                algorithm_process = Process(
+                    target=self.process_algorithm, args=(remote_path,)
+                )
                 algorithm_process.start()
                 self.processes.append(algorithm_process)
 
             for process in self.processes:
                 process.join()
-
-            print("finished")
-
+            self.processes = []
         else:
             self.process_algorithm(self.algorithm_name)
-
-        return
+        return self.files
 
     def process_algorithm(self, remote_path):
 
@@ -68,39 +53,21 @@ class AlgorithmProcessor:
 
         original_name = remote_path.split("/")[-1]
 
-        # Open file, analyze and save information
+        # Open file, analyze and save information, finally clean
         with open(os.path.join(os.getcwd(), tempname), "r") as algorithm_file:
             lines = algorithm_file.readlines()
             algorithm_content = " ".join(line for line in lines)
             algorithm_info = self.calculate_algorithm_info(lines)
 
-        self.files.append({"algorithm_name": original_name, "algorithm_content": algorithm_content,
-                           "algorithm_info": algorithm_info})
+        self.files.append(
+            {
+                "algorithm_name": original_name,
+                "algorithm_content": algorithm_content,
+                "algorithm_info": algorithm_info,
+            }
+        )
 
-        print(self.files)
-        # Cleanup
         os.remove(tempname)
-
-
-
-        # Save information to Task
-        # download_container = RunContainer(algorithm, "", download_dir=os.getcwd())
-        # download_container.create_files()
-        # download_container.download_from_rd(data=False)
-        # algorithm_content = None
-        # algorithm_info = None
-        #
-        # if download_container.temp_algorithm_file:
-        #     with open(download_container.temp_algorithm_file, "r") as algorithm_file:
-        #         lines = algorithm_file.readlines()
-        #         algorithm_content = " ".join(line for line in lines)
-        #         algorithm_info = Tasks.calculate_algorithm_info(lines)
-        #
-        # download_container.remove_files()
-        # task.algorithm_content = algorithm_content
-        # task.algorithm_info = algorithm_info
-        # task.save()
-        return
 
     @staticmethod
     def calculate_algorithm_info(lines):
@@ -130,14 +97,14 @@ class AlgorithmProcessor:
                 [word for word in stripped_newline.split(" ") if len(word) > 1]
             )
         return (
-                f"{characters} chars, {newline} line breaks, {words} words. "
-                + f'Packages: {", ".join(imports)}'
+            f"{characters} chars, {newline} line breaks, {words} words. "
+            + f'Packages: {", ".join(imports)}'
         )
 
 
 def main():
     a = AlgorithmProcessor("INT_30", "sander@wearebit.com")
-    a.process()
+    a.start_processing()
 
     return
 

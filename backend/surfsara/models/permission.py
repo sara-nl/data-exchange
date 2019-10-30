f8eacfe1a27110c28fe303ef445d371c078231cd
Sander van Wickeren <sander@bit-students.com>
2019-10-30 17:28:33 +0100

    Added one off permission to permission table

--- backend/surfsara/models/permission.py
+++ backend/surfsara/models/permission.py
@@ -5,6 +5,7 @@ class Permission(models.Model):
     USER_PERMISSION = "user permission"
     STREAM_PERMISSION = "stream permission"
     NO_PERMISSION = "no permission"
+    ONE_OFF_PERMISSION = "one off permission"
 
     algorithm = models.TextField(null=True)
     algorithm_provider = models.EmailField()
@@ -12,11 +13,12 @@ class Permission(models.Model):
     dataset_provider = models.EmailField()
     review_output = models.BooleanField(default=True)
     registered_on = models.DateTimeField(auto_now_add=True)
-    etag = models.CharField()
+    etag = models.CharField(max_length=255)
 
     permission_type = models.CharField(
         max_length=255,
         choices=[
+            (ONE_OFF_PERMISSION, "one off permisson"),
             (USER_PERMISSION, "user permission"),
             (STREAM_PERMISSION, "stream permission"),
             (NO_PERMISSION, "no permission"),

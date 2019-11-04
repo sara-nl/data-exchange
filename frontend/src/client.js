2edfa7924d4877f14ce3196d5d84eb8250654aca
Sijmen Schoon <me@sijmenschoon.nl>
2019-11-04 12:47:53 +0100

    Fix some navbar issues

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,10 +1,11 @@
 import "bootstrap";
 import * as sapper from '@sapper/app';
-import { token, email } from "./stores.ts";
+import { token, mode, email } from "./stores.ts";
 import Controller from "./api/controller.ts";
 
 token.useLocalStorage();
 email.useLocalStorage();
+mode.useLocalStorage();
 Controller.setup(sapper);
 
 sapper.start({

d53475a3232419c5cd2eb92e626152d49e54fbcf
Sander van Wickeren <sander@bit-students.com>
2019-11-07 12:11:47 +0100

    Added a new state to determine if a task is a request for stream permission

--- backend/surfsara/models/task.py
+++ backend/surfsara/models/task.py
@@ -4,6 +4,7 @@ from surfsara.models.permission import Permission
 
 
 class Task(models.Model):
+    USER_PERMISSION_REQUEST = "user_permission_request"
     REGISTERED = "registered"
     DATA_REQUESTED = "data_requested"
     ANALYZING = "analyzing_algorithm"
@@ -14,20 +15,21 @@ class Task(models.Model):
     REQUEST_REJECTED = "request_rejected"
     RELEASE_REJECTED = "release_rejected"
 
+    TASK_STATES = ((USER_PERMISSION_REQUEST, "user_permission_request"),
+                   (DATA_REQUESTED, "Data Requested"),
+                   (RUNNING, "Running"),
+                   (ANALYZING, "Analyzing_algorithm"),
+                   (SUCCESS, "Success"),
+                   (ERROR, "Error"),
+                   (OUTPUT_RELEASED, "Output Released"),
+                   (REQUEST_REJECTED, "Request Rejected"),
+                   (RELEASE_REJECTED, "Release Rejected"),
+                   )
+
     id = models.AutoField(primary_key=True)
     state = models.CharField(
         max_length=255,
-        choices=[
-            (REGISTERED, "Registered"),
-            (DATA_REQUESTED, "Data Requested"),
-            (RUNNING, "Running"),
-            (ANALYZING, "Analyzing_algorithm"),
-            (SUCCESS, "Success"),
-            (ERROR, "Error"),
-            (OUTPUT_RELEASED, "Output Released"),
-            (REQUEST_REJECTED, "Request Rejected"),
-            (RELEASE_REJECTED, "Release Rejected"),
-        ],
+        choices=TASK_STATES
     )
     author_email = models.EmailField()
     approver_email = models.EmailField()

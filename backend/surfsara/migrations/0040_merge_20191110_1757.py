32e477764885d4cd2a35d4f83c2eb23679b53e0e
Tijs Teulings <tijs.t@hotmail.com>
2019-11-10 21:37:08 +0100

    black

--- backend/surfsara/migrations/0040_merge_20191110_1757.py
+++ backend/surfsara/migrations/0040_merge_20191110_1757.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('surfsara', '0036_auto_20191107_1052'),
-        ('surfsara', '0039_auto_20191107_1948'),
+        ("surfsara", "0036_auto_20191107_1052"),
+        ("surfsara", "0039_auto_20191107_1948"),
     ]
 
-    operations = [
-    ]
+    operations = []

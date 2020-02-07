919bc1e23f6894b5678fb6282a4b88b574c6b670
Mike Kotsur <miccots@gmail.com>
2020-02-07 14:13:50 +0100

    Simplified local storage code

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,11 +1,7 @@
 import "bootstrap";
 import * as sapper from '@sapper/app';
-import { token, mode, email } from "./stores";
 import Controller from "./api/controller";
 
-token.useLocalStorage();
-email.useLocalStorage();
-mode.useLocalStorage();
 Controller.setup(sapper);
 
 sapper.start({

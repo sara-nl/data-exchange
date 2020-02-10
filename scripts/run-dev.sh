83e233c8dfea3f2484c197a1585602664f3d4f67
Mike Kotsur <miccots@gmail.com>
2020-02-10 14:24:04 +0100

    Small fix in run-dev.sh script

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -2,4 +2,4 @@
 
 # This script builds and runs development version of all services
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build "$1"
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build -d $1

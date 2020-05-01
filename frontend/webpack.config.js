71d8fe998c570ea78cce3b8492443f016e20da7e
Mike Kotsur <miccots@gmail.com>
2020-05-01 16:28:50 +0200

    Removed service worker. Related to #63

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,96 +1,90 @@
-const webpack = require('webpack');
-const path = require('path');
-const config = require('sapper/config/webpack.js');
-const preprocess = require('svelte-preprocess');
-const pkg = require('./package.json');
+const webpack = require('webpack')
+const path = require('path')
+const config = require('sapper/config/webpack.js')
+const preprocess = require('svelte-preprocess')
+const pkg = require('./package.json')
 
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 const smpConfig = {
-    granularLoaderData: true,
-};
-const smpClient = new SpeedMeasurePlugin(smpConfig);
-const smpServer = new SpeedMeasurePlugin(smpConfig);
+  granularLoaderData: true,
+}
+const smpClient = new SpeedMeasurePlugin(smpConfig)
+const smpServer = new SpeedMeasurePlugin(smpConfig)
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') };
-const extensions = ['.ts', '.mjs', '.js', '.json', '.svelte', '.html'];
-const mainFields = ['svelte', 'module', 'browser', 'main'];
+const alias = { svelte: path.resolve('node_modules', 'svelte') }
+const extensions = ['.ts', '.mjs', '.js', '.json', '.svelte', '.html']
+const mainFields = ['svelte', 'module', 'browser', 'main']
 
 module.exports = {
-    client: smpClient.wrap({
-        entry: config.client.entry(),
-        output: config.client.output(),
-        resolve: { alias, extensions, mainFields },
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader',
-                        options: {
-                            dev,
-                            preprocess: preprocess(),
-                            hydratable: true,
-                            hotReload: true,
-                        },
-                    },
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
+  client: smpClient.wrap({
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              preprocess: preprocess(),
+              hydratable: true,
+              hotReload: true,
+            },
+          },
         },
-        mode,
-        plugins: [
-            dev && new webpack.HotModuleReplacementPlugin(),
-            new webpack.DefinePlugin({
-                'process.browser': true,
-                'process.env.NODE_ENV': JSON.stringify(mode)
-            }),
-        ].filter(Boolean),
-        devtool: dev && 'eval-source-map'
-    }),
-
-    server: smpServer.wrap({
-        entry: config.server.entry(),
-        output: config.server.output(),
-        target: 'node',
-        resolve: { alias, extensions, mainFields },
-        externals: Object.keys(pkg.dependencies).concat('encoding'),
-        module: {
-            rules: [
-                {
-                    test: /\.(svelte|html)$/,
-                    use: {
-                        loader: 'svelte-loader',
-                        options: {
-                            css: false,
-                            preprocess: preprocess(),
-                            generate: 'ssr',
-                            dev
-                        }
-                    }
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ]
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
-        mode,
-        performance: {
-            hints: false // it doesn't matter if server.js is large
-        }
-    }),
+      ],
+    },
+    mode,
+    plugins: [
+      dev && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+    ].filter(Boolean),
+    devtool: dev && 'eval-source-map',
+  }),
 
-    serviceworker: {
-        entry: config.serviceworker.entry(),
-        output: config.serviceworker.output(),
-        mode,
-    }
-};
+  server: smpServer.wrap({
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              css: false,
+              preprocess: preprocess(),
+              generate: 'ssr',
+              dev,
+            },
+          },
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    mode,
+    performance: {
+      hints: false, // it doesn't matter if server.js is large
+    },
+  }),
+}

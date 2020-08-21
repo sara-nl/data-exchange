9f497789ba93edff7a96c2498c180c69165ed474
Mike Kotsur <miccots@gmail.com>
2020-08-21 15:36:12 +0200

    Changed CR naming convention

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,13 +4,7 @@
 # all services so that they use new images.
 
 # Attention! This script should be executed from Gitlab CI environment only.
-# It requires $CI_COMMIT_REF_NAME and $CI_COMMIT_SHORT_SHA variables
-# to be set!
-
-if [[ -z "$DOCKER_IMAGE_REF_NAME" ]]; then
-    echo "Must provide DOCKER_IMAGE_REF_NAME in environment" 1>&2
-    exit 1
-fi
+# It requires $DOCKER_IMAGE_TAG variable to be set!
 
 if [[ -z "$DOCKER_IMAGE_TAG" ]]; then
     echo "Must provide DOCKER_IMAGE_TAG in environment" 1>&2

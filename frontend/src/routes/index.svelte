2124f5a1f24abbf4d41374f1fc51b7911ba98e45
Mike Kotsur <miccots@gmail.com>
2020-10-02 12:39:45 +0200

    Simpler layout landing page

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -50,7 +50,10 @@
             <br />
             into the "Share with people and groups" dialogue.
           </p>
-          <p><b>4.</b> Your files should become visible on Data Exchange.</p>
+          <p>
+            <b>4.</b>
+            Your files should become visible on Data Exchange.
+          </p>
         </div>
         <div class="row w-100 mt-4 mb-3">
           <button
@@ -68,39 +71,37 @@
     </div>
   {:else}
     <div class="row my-5">
-      <div class="col ym-5 p-5 bg-lightgrey rounded-xl">
+      <div class="col-8 ym-5 p-5 bg-lightgrey rounded-xl">
         <h3 class="ym-3">How does the DataExchange work?</h3>
-        <div class="col-8 font-weight-normal p-4">
-          <p class="lead">
-            The DataExchange offers a controlled and safe third-party
-            environment where dataset and algorithm providers can safely
-            cooperate and share data. The goal of DataExchange is to encourage
-            sharing data while maintaining data sovereignty.
-          </p>
-          <p>
-            On DataExchange, owners can share their datasets while staying in
-            control. Algorithm owners can be given permissions to run on
-            datasets, but will never be physically in control of it.
-          </p>
+        <p class="lead">
+          The DataExchange offers a controlled and safe third-party environment
+          where dataset and algorithm providers can safely cooperate and share
+          data. The goal of DataExchange is to encourage sharing data while
+          maintaining data sovereignty.
+        </p>
+        <p>
+          On DataExchange, owners can share their datasets while staying in
+          control. Algorithm owners can be given permissions to run on datasets,
+          but will never be physically in control of it.
+        </p>
 
-          <h5 class="font-weight-bold">Secure container</h5>
-          <p>
-            The DataExchange provides a secure and networkless trusted
-            third-party containers in which algorithm and dataset can safely
-            interact. The dataset and algorithm are downloaded anonymously in
-            the secure container and removed when the execution has been
-            finished. Neither algorithm owner nor any other outside party will
-            have physical hold of the dataset during this process.
-          </p>
-          <h5 class="font-weight-bold">Full data sovereignty</h5>
+        <h5 class="font-weight-bold">Secure container</h5>
+        <p>
+          The DataExchange provides a secure and networkless trusted third-party
+          containers in which algorithm and dataset can safely interact. The
+          dataset and algorithm are downloaded anonymously in the secure
+          container and removed when the execution has been finished. Neither
+          algorithm owner nor any other outside party will have physical hold of
+          the dataset during this process.
+        </p>
+        <h5 class="font-weight-bold">Full data sovereignty</h5>
 
-          <p>
-            As owner of a dataset you can monitor all permissions, runs and
-            review their output. All shared datasets are stored on your personal
-            ResearchDrive and can be shared and unshared on your command at any
-            time.
-          </p>
-        </div>
+        <p>
+          As owner of a dataset you can monitor all permissions, runs and review
+          their output. All shared datasets are stored on your personal
+          ResearchDrive and can be shared and unshared on your command at any
+          time.
+        </p>
       </div>
     </div>
   {/if}
